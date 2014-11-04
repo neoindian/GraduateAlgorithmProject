96a94f9813452764fdcd98684cd4a6f8686c920d
neoindian <neoindian@tamu.edu>
2014-11-04 00:02:19 -0600

    GraphMain.cpp First Commit

---
+++ Algo629/GraphMain.cpp
@@ -0,0 +1,16 @@
+/*
+ * GraphMain.cpp
+ *
+ *  Created on: 03-Nov-2014
+ *      Author: nishant
+ */
+
+#include "GraphCreate.H"
+
+int main(int argc,char* argv[])
+{
+	Graph gObj(6,5000);
+	cout<<"Done"<<endl;
+}
+
+
